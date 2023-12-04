77c1f40b8afd5b8b1c96de55ff6dbab599f2a337
myogeshchavan97 <myogeshchavan97@gmail.com>
2023-12-04 21:21:45 +0530

    3 ways to implement protected routes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = ({ isLoggedIn }) => {
-  return <>{isLoggedIn ? <Outlet /> : <Navigate to='/' />}</>;
+  return <div>{isLoggedIn ? <Outlet /> : <Navigate to='/' />}</div>;
 };
 
 export default ProtectedRoute;
